c8a252ae3f9c5680b0b9a6c1ffb2ffacb8b58e88
S-Marimbi <marimbinjunge@gmail.com>
2024-07-12 23:45:48 +0300

    Completed pizza code challenge

---
+++ server/models.py
@@ -0,0 +1,52 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', cascade='all, delete')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address,
+            'restaurant_pizzas': [rp.to_dict() for rp in self.restaurant_pizzas]
+        }
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    ingredients = db.Column(db.String, nullable=False)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza', cascade='all, delete')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'ingredients': self.ingredients,
+        }
+
+class RestaurantPizza(db.Model):
+    __tablename__ = 'restaurant_pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'price': self.price,
+            'pizza_id': self.pizza_id,
+            'restaurant_id': self.restaurant_id,
+            'pizza': self.pizza.to_dict(),
+            'restaurant': self.restaurant.to_dict(),
+        }
